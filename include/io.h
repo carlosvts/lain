b1d19a6d0abf3d8804d7a38f72d1f0bc0620ebdd
carlosvts <carlosvtsdev@gmail.com>
2026-02-03 01:50:35 -0300

    development of io syscalls

--- include/io.h
+++ include/io.h
@@ -3,8 +3,8 @@
 LAIN_START
 
 int open_file(const char* path);
-long read_file(const char* file);
-long write_file(const char* file);
+long read_file(int fd, size_t amount);
+long write_file(int fd, size_t data);
 int close_file(int fd);
 
 LAIN_END
--- src/lib/io.c
+++ src/lib/io.c
@@ -1,4 +1,47 @@
 #include "lain.h"
 #include "io.h"
 
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
 
+// All of the above functions should be REMADE to include MORE FEATURES
+
+int open_file(const char* path)
+{
+    int fd = open(path, O_RDONLY, 0);
+    if (fd == -1)
+    {
+        // TODO change to my print later
+        return 1;
+    }
+    return fd;
+}
+
+long read_file(int fd, size_t amount)
+{
+    char buffer[sizeof(amount)];
+    int bytes = read(fd, buffer, amount);
+    if (bytes == -1)
+    {
+        return 1;
+    }
+    return bytes;
+}
+
+long write_file(int fd, size_t data)
+{
+    char buffer[sizeof(data)];
+    int bytes = write(fd, buffer, data);
+    if (bytes == -1) 
+    {
+        return 1;
+    }
+    return bytes;
+}
+
+int close_file(int fd)
+{
+    int ok = close(fd);
+    if (ok == -1) { return 1; }
+}
