922ee262cdd031f5a6fb5bb28b46674bc1138a2b
carlosvts <carlosvtsdev@gmail.com>
2026-02-05 01:22:47 -0300

    add echo.cpp and changes to makefile

---
+++ src/coreutils/echo.cpp
@@ -0,0 +1,17 @@
+#include "io.h"
+#include "strings.h"
+#include <unistd.h>
+
+int main(int argc, const char* argv[])
+{
+    for (int i = 1; i < argc; i++)
+    {
+        lain::write_file(STDOUT_FILENO, argv[i], lain::stringlen(argv[i]));
+        if (i < argc - 1)
+        {
+            lain::write_file(STDOUT_FILENO, " ", lain::stringlen(" "));
+        }
+    }
+    lain::write_file(STDOUT_FILENO, "\n", lain::stringlen("\n"));
+    return 0;
+}
